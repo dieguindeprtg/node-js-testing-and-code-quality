a3868f7f881fcf0a50fb957c48dc47f295d5737b
dieguindeprtg <dieguindeprtg@gmail.com>
2024-07-23 12:57:15 -0400

    Curso: 23/07/2024 12:57

--- Exercise Files/progress/02_07/begin/eslint.config.mjs
+++ Exercise Files/progress/02_07/begin/eslint.config.mjs
@@ -1,8 +1,16 @@
 import js from "@eslint/js";
 import globals from "globals";
+import { includeIgnoreFile } from "@eslint/compat";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const eslintIgnorePath = path.resolve(__dirname, ".eslintignore");
 
 export default [
   js.configs.recommended,
+  includeIgnoreFile(eslintIgnorePath),
   {
     languageOptions: {
         globals: {
@@ -14,6 +22,6 @@ export default [
         "no-empty": "error",
         "no-multiple-empty-lines": "warn",
         "no-var": "error",
-        "prefer-const": "error",
+        "prefer-const": "error"
     },
 }];
