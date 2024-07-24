28f333b37523167f070e1bcc7a6cf1a3b31017e9
dieguindeprtg <dieguindeprtg@gmail.com>
2024-07-24 12:17:58 -0400

    Estado de curso NodeJS Testing and Code Quality: 24/07/2024 12:17

---
+++ Exercise Files/progress/03_04/begin/lib/schema/reservation.test.js
@@ -0,0 +1,20 @@
+const Reservation = require("./reservation");
+
+describe("combineDateTime", () => {
+  it("should return an ISO date and time with valid input", () => {
+    const date = "2017/06/10";
+    const time = "06:02 AM";
+
+    const expected = "FAIL-06-10T06:02:00.000Z";
+    const actual = Reservation.combineDateTime(date, time);
+
+    expect(actual).toEqual(expected);
+  });
+
+  it("should return null on a bad date and time", () => {
+    const date = "!@#$";
+    const time = "fail";
+
+    expect(Reservation.combineDateTime(date, time)).toBeNull();
+  });
+});
