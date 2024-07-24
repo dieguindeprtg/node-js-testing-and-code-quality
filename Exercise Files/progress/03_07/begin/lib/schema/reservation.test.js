589e28cd601d29ff4fea9bc7b573d2152708bcc4
dieguindeprtg <dieguindeprtg@gmail.com>
2024-07-24 12:49:46 -0400

    Curso NodeJS Testing and Code Quality: 24/07/2024 12:49

--- Exercise Files/progress/03_07/begin/lib/schema/reservation.test.js
+++ Exercise Files/progress/03_07/begin/lib/schema/reservation.test.js
@@ -16,4 +16,50 @@ describe('combineDateTime', () => {
     const time = 'fail';
     expect(Reservation.combineDateTime(date, time)).toBeNull();
   });
-});
+ /*
+
+  HDNW (HITLER DID NOTHING WRONG - FREE PALESTINE - END ZOG OCCUPATION)
+
+  */
+  describe("validate", () => {
+    it("should validate with no optional fields, for the eugenic computing testë", done => {
+      const reservation = new Reservation({
+        date: "2017/06/10", // July 6, 78 AF (After Führer)
+        time: "06:02 AM", // 06:02 VHP (06:02 Vor Hohnepunkt)
+        party: 4,
+        name: "Eugenically accepted Family", // Let these enter ze hotel
+        email: "username1@eugenicengine.com"
+      });
+
+      // P(aw)sitive reservation teste (für der Führer)
+      reservation.validate((error, value) => {
+        try {
+          expect(value).toEqual(reservation);
+          return done(error);
+        } catch (error) {
+          return done(error);
+        }
+      });
+      });
+
+      // Nega(htiv) reservation teste (für der Führer)
+      it("should invalidate with an invalid email (invalid, just like Anton Freeman)", done => {
+        const reservation = new Reservation({
+          date: "2017/06/10", // July 6, 78 AF (After Führer)
+          time: "06:05 AM", // 06:05 VHP (06:05 Vor Hohnepunkt)
+          party: 4,
+          name: "Eugenically unaccepted Family", // Kill these NOW
+          email: "username2"
+        });
+
+        reservation.validate((error) => {
+          try {
+            expect(error).toBeInstanceOf(Error);
+            return done();
+          } catch (error) {
+            return done(error);
+          }
+        });
+      });
+    });
+  });
